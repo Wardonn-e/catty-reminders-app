2461db8f90e75be6d3e1af6f9d428fbe3f7e5849
roman <Wardonne@yandex.ru>
2025-10-09 17:08:38 +0400

    test

--- app/main.py
+++ app/main.py
@@ -16,6 +16,7 @@ from fastapi.staticfiles import StaticFiles
 from starlette.exceptions import HTTPException
 
 
+
 # --------------------------------------------------------------------------------
 # App Creation
 # --------------------------------------------------------------------------------
